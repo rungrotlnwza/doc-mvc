e813e5f1755bd389e3c80ae3b69c47efc32592f3
rungrotlnwza <rungrot.muangchalerm@gmail.com>
2024-11-17 04:33:32 +0700

    navbar update

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,21 @@
-const randomversion = Math.random(); // สร้างเลขสุ่ม
-fetch('view/navbar.html?v=$(randomversion)') // เปลี่ยน path ให้ตรงกับตำแหน่งที่ถูกต้อง
-    .then(response => {
+const randomVersion = Math.random(); // สร้างเลขสุ่ม
+
+fetch('view/navbar.html?v=' + randomVersion) // เปลี่ยน path ให้ตรงกับตำแหน่งที่ถูกต้อง
+    .then(function(response) {
         if (!response.ok) {
-            throw new Error(`Failed to load navbar: ${response.status} ${response.statusText}`);
+            throw new Error('Failed to load navbar: ' + response.status + ' ' + response.statusText);
         }
         return response.text();
     })
-    .then(html => {
+    .then(function(html) {
         // แสดง Navbar ใน #navbar-container
-        const navbarContainer = document.getElementById('navbar');
+        var navbarContainer = document.getElementById('navbar');
         if (navbarContainer) {
             navbarContainer.innerHTML = html;
         } else {
             console.error('Error: Element with id "navbar-container" not found.');
         }
     })
-    .catch(error => {
+    .catch(function(error) {
         console.error('Error loading navbar:', error);
     });
--- view/header.php
+++ view/header.php
@@ -7,7 +7,7 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
